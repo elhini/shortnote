bfb4402c24330a719116bb3283ff5d1238268ddc
Artem Guslov <elhini@yandex-team.ru>
2019-04-25 00:11:53 +0300

    select item on open, add delete btn

--- src/App.js
+++ src/App.js
@@ -7,39 +7,85 @@ import List from './components/List/List';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    var items = ls.get();
+    var emptyItem = this.buildEmptyItem();
+    items.unshift(emptyItem);
     this.state = {
-      items: ls.get(),
-      item: {id: 0, title: '', text: ''}
+      items: items
     };
+    this.onOpenNew = this.onOpenNew.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-    this.onEditItem = this.onEditItem.bind(this);
+    this.onOpenItem = this.onOpenItem.bind(this);
+    this.onDeleteItem = this.onDeleteItem.bind(this);
+  }
+
+  findEmptyItem(){
+    return this.state.items.find(i => i.id === 0);
+  }
+
+  buildEmptyItem(){
+    return {id: 0, title: '', text: '', opened: true};
+  }
+
+  onOpenNew(){
+    var items = this.state.items;
+    var emptyItem = this.findEmptyItem();
+    if (!emptyItem){
+      emptyItem = this.buildEmptyItem();
+      items.unshift(emptyItem);
+    }
+    items.forEach(i => i.opened = i.id === 0);
+    this.setState({items: items});
   }
 
   onSubmit(e){
     var form = e.target;
     var idFromForm = parseInt(form.id.value);
     var id = idFromForm || Date.now();
-    var newItem = {id: id, title: form.title.value, text: form.text.value};
+    var itemFromForm = {id: id, title: form.title.value, text: form.text.value, opened: true};
     var items = this.state.items;
     if (idFromForm){
       items = items.filter(i => i.id !== idFromForm);
     }
-    items.push(newItem);
+    else {
+      this.findEmptyItem().opened = false;
+    }
+    items.push(itemFromForm);
     items.sort((a, b) => a.id - b.id);
-    ls.set(items);
+    this.saveToStore(items);
     this.setState({items: items});
     e.preventDefault();
   }
 
-  onEditItem(item){
-    this.setState({item: item});
+  saveToStore(items){
+    ls.set(items.filter(i => i.id > 0));
+  }
+
+  onOpenItem(item){
+    var items = this.state.items;
+    items.forEach(i => i.opened = i.id === item.id);
+    this.setState({items: items});
+  }
+
+  onDeleteItem(item){
+    var items = this.state.items;
+    if (item.opened){
+      this.findEmptyItem().opened = true;
+    }
+    items = items.filter(i => i.id !== item.id);
+    this.saveToStore(items);
+    this.setState({items: items});
   }
 
   render(){
+    var openedItem = this.state.items.find(i => i.opened);
     return (
       <div id="App">
-        <Form item={this.state.item} onSubmit={this.onSubmit}></Form>
-        <List items={this.state.items} onEditItem={this.onEditItem}></List>
+        <h1>ShortNote</h1>
+        <button id="openNew" onClick={this.onOpenNew}>Open new</button>
+        <Form item={openedItem} onSubmit={this.onSubmit}></Form>
+        <h2>List</h2>
+        <List items={this.state.items} onOpenItem={this.onOpenItem} onDeleteItem={this.onDeleteItem}></List>
       </div>
     );
   }
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,10 +2,18 @@ import React from 'react';
 import './List.css';
 
 export default class List extends React.Component {
+  onDelete(e, item){
+    e.stopPropagation(); 
+    this.props.onDeleteItem(item);
+  }
+
   buildNodes(){
     return this.props.items.map(function(item){
+      var opened = item.opened ? 'opened' : '';
+      var deleteBtn = item.id > 0 ? <button className="delete" onClick={(e) => this.onDelete(e, item)}>X</button> : null;
       return (
-        <li key={item.id} data-id={item.id} onClick={() => this.props.onEditItem(item)}>
+        <li key={item.id} data-id={item.id} className={opened} onClick={() => this.props.onOpenItem(item)}>
+          {deleteBtn}
           <div className="title">{item.title}</div>
           <div className="text">{item.text}</div>
         </li>
