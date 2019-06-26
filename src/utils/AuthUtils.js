a97816fbfd179b03dbbcabb7f79faf49ec69de56
Artem Guslov <elhini@yandex-team.ru>
2019-06-26 23:10:45 +0300

    implement auth utils for session handling

---
+++ src/utils/AuthUtils.js
@@ -0,0 +1,31 @@
+import LocalStorage from "./LocalStorage";
+import UsersApiClient from '../api-clients/users';
+    
+export default class AuthUtils {
+    static isLoggedIn() {
+        return !!this.getSession();
+    }
+    
+    static getSession() {
+        return this.session || JSON.parse(LocalStorage.get('session'));
+    }
+
+    static login(login, password, cb) {
+        (new UsersApiClient()).login(login, password, (session) => {
+            this.setSession(session);
+            cb(session);
+        });
+    }
+
+    static logout(cb) {
+        (new UsersApiClient()).logout(() => {
+            this.setSession(null);
+            cb();
+        });
+    }
+    
+    static setSession(session) {
+        LocalStorage.set('session', JSON.stringify(session));
+        this.session = session;
+    }
+};
