12b026ee11ad3147e06d92ace7cb33c98f17611f
Artem Guslov <elhini@yandex-team.ru>
2019-06-28 08:58:29 +0300

    use material-ui, restyle delete btn, add icon

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './List.css';
 import DateUtils from '../../utils/DateUtils';
 import { Link } from "react-router-dom";
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 export default class List extends React.Component {
   constructor(props) {
@@ -28,10 +30,12 @@ export default class List extends React.Component {
       var opened = this.props.item && this.props.item._id === item._id ? 'opened' : '';
       var tags = (item.tags || []).map(tag => <span className="tag" data-id={tag.value} key={tag.value}>{tag.label}</span>);
       var aside = (<div className="aside">
-        <span className="dateOfCreate">{DateUtils.toStr(item.dateOfCreate)}</span>
-        <span className="dateOfUpdate">{DateUtils.toStr(item.dateOfUpdate)}</span>
-        <button className="delete" onClick={(e) => window.confirm('Delete this item?') && this.onDelete(e, item)}>X</button>
-        <div className="tags">{tags}</div>
+        <div className="aside-inner">
+          <span className="dateOfCreate">{DateUtils.toStr(item.dateOfCreate)}</span>
+          <span className="dateOfUpdate">{DateUtils.toStr(item.dateOfUpdate)}</span>
+          <div className="tags">{tags}</div>
+        </div>
+        <IconButton className="delete" onClick={(e) => window.confirm('Delete this item?') && this.onDelete(e, item)}><DeleteIcon /></IconButton>
       </div>);
       return (
         <li key={item._id} className={opened}>
