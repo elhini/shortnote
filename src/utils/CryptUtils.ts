bd27c55d950bd9356f0538c44975f138ead2289d
Artem Guslov <elhini@yandex-team.ru>
2019-08-13 00:43:38 +0300

    use arrow functions in crypt utils

--- src/utils/CryptUtils.ts
+++ src/utils/CryptUtils.ts
@@ -2,18 +2,18 @@ import * as bcrypt from 'bcrypt';
 
 export default class CryptUtils {
   static cryptPassword(password: string, callback: (err: Error, hash?: string) => void) {
-    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.genSalt(10, (err, salt) => {
       if (err) 
         return callback(err);
-      bcrypt.hash(password, salt, function(err, hash) {
+      bcrypt.hash(password, salt, (err, hash) => {
         return callback(err, hash);
       });
     });
   }
 
   static comparePassword(plainPass: string, hashword: string, callback: (err: Error, isPasswordMatch: boolean) => void) {
-    bcrypt.compare(plainPass, hashword, function(err, isPasswordMatch) {   
-        return callback(err, isPasswordMatch);
+    bcrypt.compare(plainPass, hashword, (err, isPasswordMatch) => {   
+      return callback(err, isPasswordMatch);
     });
   }
 }
