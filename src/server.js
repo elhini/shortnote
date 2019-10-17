17aff61093e0b5a922e54df8e56e781783eba380
Artem Guslov <elhini@yandex-team.ru>
2019-10-18 01:22:51 +0300

    use CORS filtering for client's host

--- src/server.js
+++ src/server.js
@@ -6,10 +6,12 @@ const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const cookieParser   = require("cookie-parser");
+const cors           = require('cors');
 const app            = express();
 const port           = process.env.PORT || 8000;
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors({ origin: "shortnote.elhini.now.sh" }));
 const dbURL = process.env.HOST === 'localhost' ? require('./config/db').url : process.env.MONGODB_URI;
 MongoClient.connect(dbURL, { useNewUrlParser: true }, (err, client) => {
     if (err) {
