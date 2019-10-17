8f0ff88fd7cb91e74419fc69a7ac6326f5411c6b
Artem Guslov <elhini@yandex-team.ru>
2019-10-18 00:32:57 +0300

    refactor start server commands

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
+console.log('process.env.HOST', process.env.HOST);
+if (!process.env.HOST){
+    return;
+}
 const express        = require('express');
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const cookieParser   = require("cookie-parser");
-const dbConfig       = require('./config/db');
 const app            = express();
 const port           = 8000;
 app.use(bodyParser.json());
 app.use(cookieParser());
-MongoClient.connect(dbConfig.url, { useNewUrlParser: true }, (err, client) => {
+const dbURL = process.env.HOST === 'localhost' ? require('./config/db').url : process.env.MONGODB_URI;
+console.log('dbURL', dbURL);
+if (!dbURL){
+    return;
+}
+MongoClient.connect(dbURL, { useNewUrlParser: true }, (err, client) => {
     if (err) {
         console.log(err);
     }
