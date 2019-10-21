80a151a5e03b512b476ae8b7d6f9ad3007795e14
Artem Guslov <elhini@yandex-team.ru>
2019-10-22 00:38:13 +0300

    Updates

---
+++ precache-manifest.730a2573fe70e2b23c19876406ee3155.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05e7a4ebeb03d1a2a11d6b35e12ccc7",
+    "url": "/shortnote/index.html"
+  },
+  {
+    "revision": "d237ac3a8c1b413337e8",
+    "url": "/shortnote/static/css/main.bd5ca634.chunk.css"
+  },
+  {
+    "revision": "f1adfc45c28e078a89c0",
+    "url": "/shortnote/static/js/2.4d2966a6.chunk.js"
+  },
+  {
+    "revision": "d237ac3a8c1b413337e8",
+    "url": "/shortnote/static/js/main.3c60c5c1.chunk.js"
+  },
+  {
+    "revision": "f0fd716f79f8de2b22f7",
+    "url": "/shortnote/static/js/runtime~main.7e487c32.js"
+  }
+]);
